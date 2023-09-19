0f7d570bb34ad7a63fa3dd277205fb48916f1b01
Samuel Galindo <samuel.galindo@correounivalle.edu.co>
2023-09-19 18:47:30 -0500

    creación de los archivos para el desarrollo

---
+++ ejercicio1.rkt
@@ -0,0 +1,14 @@
+#|
+Fundamentos de Interpretación y Compilación de Lenguajes de Programación
+750017C - G01
+=======
+Taller 2: Abstracción de datos y sintáxis abstracta
+Ejercicio 1: Gramatica BNF
+
+Autores:
+Samuel Galindo 2177491
+Nicolás Herrera 2182551
+Christian Vargas 2179172
+|#
+
+#lang eopl
---
+++ ejercicio3.rkt
@@ -0,0 +1,15 @@
+#|
+Fundamentos de Interpretación y Compilación de Lenguajes de Programación
+750017C - G01
+=======
+Taller 2: Abstracción de datos y sintáxis abstracta
+Ejercicio 3: Evaluación de Instancias SAT
+
+Autores:
+Samuel Galindo 2177491
+Nicolás Herrera 2182551
+Christian Vargas 2179172
+|#
+
+#lang eopl
+
