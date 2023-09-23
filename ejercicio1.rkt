ca68aa97bda144d501e3becbe4b9a7d3d7d6cebb
Christian Vargas <vargas.christian@correounivalle.edu.co>
2023-09-23 16:37:57 -0500

    Redefinición de constructores, predicados y extractores basados en la estructura de la gramatica que escogimos

--- ejercicio1.rkt
+++ ejercicio1.rkt
@@ -22,91 +22,211 @@
         (get-element-at-index (cdr list-of-elements) (- index 1))])))
 
 
-(define or-exp
-  (lambda (list-of-operands)
-    (cond
-
-      [(or
-        (not ((list-of number?) list-of-operands))
-        (eqv? (length-of-list list-of-operands) 0))
-       (eopl:error 'IllegalArgument "Para construir una expresión OR, la lista ~s debe ser no vacía y solo debe contener números" list-of-operands)]
-
-      [(eqv? (length-of-list list-of-operands) 1)
-       (cons (car list-of-operands) empty)]
-
-      [else
-       (cons
-        (car list-of-operands)
-        (cons "OR" (or-exp (cdr list-of-operands))))])))
-
-
 (define or-exp?
   (lambda (exp)
     (cond
+      
+      [(not (list? exp))
+       #f]
+
+      [(null? exp)
+       #t]
 
       [(and
         (eqv? (length-of-list exp) 1)
         (number? (car exp)))
        #t]
 
-      [(or
-        (eqv? (length-of-list exp) 0)
-        (not (eqv? (cadr exp) "OR")))
-       #f]
-
-
       [else
        (and
         (number? (car exp))
+        (eqv? (cadr exp) "OR")
+        (not (null? (cddr exp)))
         (or-exp? (cddr exp)))])))
+
+;; Pruebas
+(eqv? (or-exp? '()) #t)
+(eqv? (or-exp? '(1)) #t)
+(eqv? (or-exp? '(1 "OR" -1)) #t)
+(eqv? (or-exp? '(1 "OR" -1 "OR" -5)) #t)
+(eqv? (or-exp? '(1 "OR" -1 -5)) #f)
+(eqv? (or-exp? '(1 "OR" -1 "OR" (-5 "OR" 2))) #f)
+(eqv? (or-exp? '(1 "OR")) #f)
+
+
+(define or-exp
+  (lambda (num another-or-exp)
+    (cond
+      
+      [(not (number? num))
+       (eopl:error 'IllegalArgumentError "El argumento ~s debe ser un número" num)]
+
+      [(not (or-exp? another-or-exp))
+       (eopl:error 'IllegalArgumentError "El argumento ~s debe ser una expresion OR" another-or-exp)]
+
+      [(null? another-or-exp)
+       (cons num empty)]
+
+      [else
+       (cons num
+             (cons "OR" another-or-exp))])))
+
+;; Pruebas
+(define or-exp1 (or-exp 1 empty))
+(define or-exp2 (or-exp 1 (or-exp 2 empty)))
+(define or-exp3 (or-exp 1 (or-exp 2 (or-exp -3 empty))))
+;(define or-exp4 (or-exp 1 (or-exp 2 (or-exp -3 4))))
+
+
+(define or-exp->varlist
+  (lambda (an-or-exp)
+    (cond
+      
+      [(not (or-exp? an-or-exp))
+       (eopl:error 'IllegalArgumentError "El argumento ~s debe ser una expresion OR" an-or-exp)]
+
+      [(eqv? (length-of-list an-or-exp) 1)
+       (cons (car an-or-exp) empty)]
+
+      [else
+       (cons (car an-or-exp) (or-exp->varlist (cddr an-or-exp)))])))
+
+;; Pruebas
+(or-exp->varlist or-exp1)
+(or-exp->varlist or-exp2)
+(or-exp->varlist or-exp3)
+
+
+(define and-exp?
+  (lambda (exp)
+    (cond
+      
+      [(not (list? exp))
+       #f]
+
+      [(null? exp)
+       #t]
+
+      [(and
+        (eqv? (length-of-list exp) 1)
+        (or-exp? (car exp)))
+       #t]
+
+      [else
+       (and
+        (or-exp? (car exp))
+        (eqv? (cadr exp) "AND")
+        (not (null? (cddr exp)))
+        (and-exp? (cddr exp)))])))
+
+;; Pruebas
+(eqv? (and-exp? '() ) #t)
+(eqv? (and-exp? '((1)) ) #t)
+(eqv? (and-exp? '((1 "OR" -1)) ) #t)
+(eqv? (and-exp? '((1 "OR" -1 "OR" 3) "AND" (2)) ) #t)
+(eqv? (and-exp? '((1 "OR" -1) "AND" (2 "OR" -2)) ) #t)
+(eqv? (and-exp? '((1 "OR" -1 "OR")) ) #f)
+(eqv? (and-exp? '((1 "OR" -1) "AND" (2 2)) ) #f)
+(eqv? (and-exp? '((1 "OR" -1) "AND" (2 "OR" -2) "AND") ) #f)
 
 
 (define and-exp
-  (lambda (list-of-operands)
+  (lambda (an-or-exp another-and-exp)
     (cond
 
       [(or
-        (not ((list-of or-exp?) list-of-operands))
-        (eqv? (length-of-list list-of-operands) 0))
-       (eopl:error 'IllegalArgument "Para construir una expresión AND, la lista ~s debe ser no vacía y solo debe contener expresiones OR" list-of-operands)]
+        (not (or-exp? an-or-exp))
+        (null? an-or-exp))
+       (eopl:error 'IllegalArgumentError "El argumento ~s debe ser una expresión OR válida y no vacía" an-or-exp)]
 
-      [(eqv? (length-of-list list-of-operands) 1)
-       (cons (car list-of-operands) empty)]
+      [(not (and-exp? another-and-exp))
+       (eopl:error 'IllegalArgumentError "El argumento ~s debe ser una expresión AND" another-and-exp)]
+
+      [(null? another-and-exp)
+       (cons an-or-exp empty)]
 
       [else
-       (cons
-        (car list-of-operands)
-        (cons "AND" (and-exp (cdr list-of-operands))))])))
+       (cons an-or-exp
+             (cons "AND" another-and-exp))])))
+
+;; Pruebas
+(define and-exp1 (and-exp or-exp1 empty))
+(define and-exp2 (and-exp or-exp2 and-exp1))
+(define and-exp3 (and-exp or-exp3 '((4 "OR" -2))))
+(define and-exp4 (and-exp '(1) (and-exp '(-1 "OR" -3) and-exp3)))
+;(define and-exp5 (and-exp '(1 "OR") empty))
+;(define and-exp6 (and-exp empty empty))
+;(define and-exp7 (and-exp or-exp3 '((1 "OR" 2) "AND")))
 
 
-;(define and-exp?
- ; (lambda (exp)
-  ;  (cond
+(define and-exp->clauses
+  (lambda (an-and-exp)
+  (cond
+    
+    [(not (and-exp? an-and-exp))
+     (eopl:error 'IllegalArgumentError "El argumento ~s debe ser una expresión AND" an-and-exp)]
 
-   ;   [(and
-    ;    (eqv? (length-of-list exp) 1)
-     ;   (or-exp? exp))
-      ;  #t]
+    
+     [(eqv? (length-of-list an-and-exp) 1)
+      (cons (car an-and-exp) empty)]
 
-      ;[(or
-       ; (eqv? (length-of-list exp) 0)
-        ;(not (eqv? (cadr exp) "AND")))
-        ;#f]
+     [else
+      (cons (car an-and-exp) (and-exp->clauses (cddr an-and-exp)))])))
 
-      ;[else
-      ; (and
-       ; (or-exp? (car exp))
-        ;(
+;; Pruebas
+(and-exp->clauses and-exp1)
+(and-exp->clauses and-exp2)
+(and-exp->clauses and-exp3)
+(and-exp->clauses and-exp4)
+
+
+(define fnc
+  (lambda (num-of-var an-and-exp)
+    (cond
+
+      [(not (number? num-of-var))
+       (eopl:error 'IllegalArgumentError "El argumento ~s debe ser un número" num-of-var)]
       
+      [(not (and-exp? an-and-exp))
+       (eopl:error 'IllegalArgumentError "El argumento ~s debe ser una expresión AND" an-and-exp)]
+      
+      [else
+       (list "FNC" num-of-var an-and-exp)])))
+
+;; Pruebas
+(define fnc1 (fnc 1 and-exp1))
+(define fnc2 (fnc 3 '((-1 "OR" 2 "OR" -3) "AND" (3 "OR" 1) "AND" (2))))
+(define fnc3 (fnc 4 and-exp4))
+;(define fnc4 (fnc "AND" and-exp1))
+;(define fnc5 (fnc 2 or-exp3))
+;(define fnc6 (fnc 3 '((-1 "OR" 2 "OR" -3) "AND" (3 "OR" 1) "AND")))
+;(define fnc7 (fnc 0 (and-exp '() '())))
+
+
+(define fnc->var
+  (lambda (fnc-exp)
+    (get-element-at-index fnc-exp 1)))
+
+;; Pruebas
+(fnc->var fnc1)
+(fnc->var fnc2)
+(fnc->var fnc3)
+
+
+(define fnc->clauses
+  (lambda (fnc-exp)
+    (and-exp->clauses (get-element-at-index fnc-exp 2))))
+
+
+;; Pruebas
+(fnc->clauses fnc1)
+(fnc->clauses fnc2)
+(fnc->clauses fnc3)
 
 
 
-;(define fnc
- ; (lambda (num-of-var and-statement)
-  ;  (list 'FNC num-of-var (and-statement))))
-
-
-
+      
+      
 
 
 
