1d84b58683bd869a544f9fcc224c17862dc14d66
Nicolas Herrera <herreran903@gmail.com>
2023-09-23 10:58:15 -0500

    Primera version de los constructores y extractores

--- ejercicio1.rkt
+++ ejercicio1.rkt
@@ -12,3 +12,42 @@ Christian Vargas 2179172
 |#
 
 #lang eopl
+
+(define expr-fnc '(FNC 4 ((1 or 2 or -4) and (2 or 4 or 3) and (-2) and (1))))
+
+(define FNC
+  (lambda (int exp) (list 'FNC int exp)))
+
+(define or
+  (lambda (int or)
+    (if (null? or)
+        (list int)
+        (append (list int 'V) or))))
+
+(define and
+  (lambda (or exp)
+    (if(null? exp)
+       (list or)
+       (cons or(cons 'AND exp)))))
+
+(define fnc->var
+  (lambda (fnc)
+    (cadr fnc)))
+
+(define fnc->clausula
+  (lambda (fnc)
+    (cond
+    [(null? fnc) '()]
+    [(list? (car fnc))
+     (cons (car fnc) (fnc->clausula (cdr fnc)))]
+    [else (fnc->clausula (cdr fnc))])))
+
+(define or->varlist
+  (lambda (or)
+    (if (null? or)
+        '()
+        (cond
+          [(number? (car or)) (cons(car or)(or->varlist(cdr or)))]
+          [else (or->varlist(cdr or))]))))
+
+(FNC 4 (and (or 2 '()) (and (or 2 (or 3 '())) (or 1 (or -3 '())))))
