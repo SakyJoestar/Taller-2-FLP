fcb74f653f531be9e9ef18abf10e4a71dc438464
Samuel Galindo <samuel.galindo@correounivalle.edu.co>
2023-09-30 12:57:51 -0500

    Ejercicio 3 solucion 1

--- ejercicio3.rkt
+++ ejercicio3.rkt
@@ -16,3 +16,75 @@ Christian Vargas 2179172
 
 (require "ejercicio1.rkt")
 (require "ejercicio2.rkt")
+
+;; ****************************************** Funciones propias ******************************************
+
+;; Funcion my-append que ya sabemos que hace
+
+(define my-append 
+  (lambda (lst1 lst2)
+  (if (null? lst1)
+      lst2
+      (cons (car lst1) (my-append (cdr lst1) lst2))))
+  )
+
+;; Función map que ya sabemos que hace tambien
+(define my-map
+  (lambda (proc lst)
+  (if (null? lst)
+      '()
+      (cons (proc (car lst))
+            (my-map proc (cdr lst)))))
+  )
+;; Función que genera todas las combinaciones
+(define combinations
+  (lambda (n)
+  (if (= n 0)
+      (list '())
+      (my-append (my-map (lambda (rest) (cons #t rest)) (combinations (- n 1)))
+              (my-map (lambda (rest) (cons #f rest)) (combinations (- n 1)))
+              )
+      )
+    )
+  )
+
+
+;;Función que retorna #t o #f para una posible combincación y una expresion FNC.
+
+(define solve-FNC
+  (lambda (exp lista)
+  (cond
+    [(null? exp) #t] ;; caso cuando un or queda vacio
+    [(number? exp) (if (> exp 0) (get-element-at-index lista (- exp 1)) ;; caso cuando un or es un numero positivo
+                                 (not (get-element-at-index lista (- (abs exp) 1)))  ;; caso cuando un or es un numero negativo
+                                                  )]
+    [(list? exp)
+     (cond
+       [(= (length exp) 1) (solve-FNC (car exp) lista)] ;; caso cuando exp es una lista de tamano 1 (cuando solo queda una expresion or dentro del and)
+       [(number? (car exp)) (or (solve-FNC (car exp) lista) (solve-FNC (cdr exp) lista))] ;; cuando el primer elemento de exp es un numero (expresion or)
+       [(list? (car exp)) (and (solve-FNC (car exp) lista) (solve-FNC (cdr exp) lista))] ;; cuando el primer elemento es una lista (exp es una expresion and)
+       )])))
+
+(define l1 '((1 -2) (3 -4) (-5)))
+(solve-FNC l1 '(#t #t #f #f #t))
+
+
+;;Función EvaluarSat que pasa los datos a solSAT para dar una respuesta
+(define EVALUARSAT
+ (lambda (fnc-e)
+ (travelSols (combinations (car fnc-e)) (cdr fnc-e))
+       ))
+
+;;Función solSat que recorre las combinaciones
+(define travelSols
+  (lambda (lbs exp)
+    (cond
+      [(null? lbs) (cons 'insatisfactible '('()) )]
+      [else (if (solve-FNC exp (car lbs)) (cons 'satisfactible (list (car lbs)))
+        (travelSols (cdr lbs) exp))]
+      )))
+
+;; ejemplos profesor
+(EVALUARSAT '(4 ((1 -2 3 4) (-2 3) (-1 -2 -3) (3 4) (2)))) ;(satisfactible (#f #t #t #t))
+(EVALUARSAT '(2 ((1 2) (-1) (-2)))) ;(insatisfactible '())
+ 
